ef71363796341e995c8089deb3e610192d59c1e5
amlola88 <amlopezla@cesde.net>
2023-06-29 11:43:56 -0500

    Creamos la funcion o metodo post para enviar respuesta a traves de formulario

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormGroup, FormBuilder} from '@angular/forms'
+import { MercanciaService } from '../servicios/mercancia.service';
 
 @Component({
   selector: 'app-formulario',
@@ -9,7 +10,7 @@ import {FormGroup, FormBuilder} from '@angular/forms'
 export class FormularioComponent {
   public formulario:FormGroup
 
-  public constructor(public fabricaDatos:FormBuilder){
+  public constructor(public fabricaDatos:FormBuilder, public peticion:MercanciaService ){
     this.formulario=this.inicializarFormulario()
   }
 
@@ -17,12 +18,23 @@ export class FormularioComponent {
     return this.fabricaDatos.group({
       nombre:[''],
       descripcion:[''],
-      fecha:[''],
+      fechaDeEntrada:[''],
       motivoDevolucion:[''],
       volumen:['']
     })
   }
 
-  public capturarDatos():void{}
+  public capturarDatos():void{
+    let datos= this.formulario.value
+    let zona={
+      id:1
+    }
+    datos.zona=zona
+    console.log (datos)
+    this.peticion.registrarMercancia(datos)
+    .subscribe(respuesta=>{
+      console.log(respuesta)
+    })
+  }
 
 }
--- src/app/servicios/mercancia.service.ts
+++ src/app/servicios/mercancia.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,8 +8,28 @@ import { Observable } from 'rxjs';
 export class MercanciaService {
 
   constructor(public servicio:HttpClient) { }
+/**Asi creamos la opcion para registrar desde el formulario.  Creamos una funcion en este caso
+ registrarMercancia con la que puedo hacer la peticion post y creamos una variable url donde guardamos 
+ el ep o url que es la ruta a la BD y luego retornamos el componente en este caso servicio
+ llamamos el metodo post que es el que nos permite llevar datos a la BD y entre parentesis
+ capturamos o llamamos las variables que creamos en el servicio (url, dato) 
 
-  public registrarMercancia(){}
+ Vinculamos front con back por medio de la direccion url de la BD.
+ */
+  public registrarMercancia(dato:any):Observable<any>{
+   /* Esto lo creamos para tratar de buscar un error, sin embargo lo dejamos porque
+   es una forma de habilitar una cabecera.  Sin embargo en el momento el error solo era que
+   faltaba el http// en la direccion url.
+   const httpOptions={
+      headers:new HttpHeaders({
+        "Access-Control-Allow-Origin":"*"
+      })
+    }*/
+    let url="http://localhost:8081/api/v1/mercancias"
+    return  this.servicio.post(url,dato,)
+
+
+  }
   public buscarMercancias():Observable<any>{
     let url="http://localhost:8081/api/v1/mercancias"
     return this.servicio.get(url)
